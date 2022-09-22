d7321df8b53f4d303caa2d503ad5a992836e3080
ggss-dev <gustavogss.jp@gmail.com>
2022-09-22 20:12:57 -0300

    Projeto Finalizado

---
+++ README.md
@@ -0,0 +1,40 @@
+# Projeto Pixel-Arts :rocket:
+
+## REQUISITOS OBRIGATÓRIOS :robot:
+
+- [x] 1 - Adicione à página o título "Paleta de Cores".
+
+- [x] 2 - Adicione à página uma paleta de quatro cores distintas.
+
+- [x] 3 - Adicione a cor **preta** como a primeira cor da paleta de cores.
+
+- [x] 4 - Adicione à página um quadro de pixels, com 25 pixels.
+
+- [x] 5 - Faça com que cada elemento do quadro de pixels possua 40 pixels de largura, 40 pixels de altura e seja delimitado por uma borda preta de 1 pixel.
+
+- [x] 6 - Defina a cor preta como cor inicial. Ao carregar a página, a cor preta já deve estar selecionada para pintar os pixels
+
+- [x] 7 - Clicar em uma das cores da paleta faz com que ela seja selecionada e utilizada para preencher os pixels no quadro.
+
+- [x] 8 - Clicar em um pixel dentro do quadro após selecionar uma cor na paleta faz com que o pixel seja preenchido com a cor selecionada.
+
+- [x] 9 - Crie um botão que, ao ser clicado, limpa o quadro preenchendo a cor de todos seus pixels com branco.
+
+
+## REQUISITOS BÔNUS :medal_military:
+
+- [x] 10 - Faça o quadro de pixels ter seu tamanho definido pela pessoa usuária.
+
+- [x] 11 - Limite o tamanho mínimo e máximo do board.
+
+- [x] 12 - Faça com que as cores da paleta sejam geradas aleatoriamente ao carregar a página.
+
+## TECNOLOGIA E FERRAMENTAS UTILIZADAS :robot:
+
+- VsCode
+- HTML5
+- CSS3
+- JavaScript
+
+## DEPLOY :runner:
+
---
+++ script.js
@@ -0,0 +1,131 @@
+const colorList = document.getElementById('color-palette');
+const pixelBoard = document.getElementById('pixel-board');
+const inputBoard = document.getElementById('board-size');
+const btnVQV = document.getElementById('generate-board');
+const btnClear = document.getElementById('clear-board');
+const firstBlock = document.createElement('div');
+const secondBlock = document.createElement('div');
+const thirdBlock = document.createElement('div');
+const fourBlock = document.createElement('div');
+
+function createColor() {
+  firstBlock.id = 'black';
+  firstBlock.className = 'color';
+  firstBlock.style.background = 'black';
+  colorList.appendChild(firstBlock);
+  secondBlock.id = 'red';
+  secondBlock.className = 'color';
+  secondBlock.style.background = 'red';
+  colorList.appendChild(secondBlock);
+  thirdBlock.id = 'blue';
+  thirdBlock.style.background = 'blue';
+  thirdBlock.className = 'color';
+  colorList.appendChild(thirdBlock);
+  fourBlock.id = 'green';
+  fourBlock.style.background = 'green';
+  fourBlock.className = 'color';
+  colorList.appendChild(fourBlock);
+}
+createColor();
+
+function selectFirstColor() {
+  if (firstBlock.style.background === 'black') {
+    firstBlock.className = 'color selected';
+  } else {
+    firstBlock.className = 'color';
+  }
+}
+selectFirstColor();
+
+function createPixels() {
+  for (let lines = 0; lines < 5; lines += 1) {
+    const line = document.createElement('div');
+    line.className = 'lines';
+    pixelBoard.appendChild(line);
+    for (let cell = 0; cell < 5; cell += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.id = 'cell';
+      line.appendChild(pixel);
+    }
+  }
+}
+createPixels();
+
+function selectColor(event) {
+  const colorSelect = document.querySelector('.selected');
+  colorSelect.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+firstBlock.addEventListener('click', selectColor);
+secondBlock.addEventListener('click', selectColor);
+thirdBlock.addEventListener('click', selectColor);
+fourBlock.addEventListener('click', selectColor);
+
+function selectPixel(event) {
+  const painted = document.querySelector('.selected');
+  const evento = event;
+  evento.target.style.backgroundColor = painted.style.background;
+}
+pixelBoard.addEventListener('click', selectPixel);
+
+const pixels = document.querySelectorAll('.pixel');
+function eventCreateClear() {
+  for (let lines = 0; lines < 5; lines += 1) {
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.background = 'white';
+    }
+  }
+}
+btnClear.addEventListener('click', eventCreateClear);
+
+function removeBoard() {
+  while (pixelBoard.firstChild) {
+    pixelBoard.removeChild(pixelBoard.firstChild);
+  }
+}
+
+function inputSizeCheck() {
+  if (inputBoard.value === '') {
+    alert('Board inválido!');
+  } else if (inputBoard.value < 5) {
+    inputBoard.value = 5;
+  } else if (inputBoard.value > 50) {
+    inputBoard.value = 50;
+  }
+  return inputBoard.value;
+}
+
+function createBoard() {
+  const size = inputBoard.value;
+  for (let lines = 1; lines <= size; lines += 1) {
+    const line = document.createElement('div');
+    line.className = 'lines';
+    pixelBoard.appendChild(line);
+    for (let cell = 1; cell <= size; cell += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixel.id = 'cell';
+      line.appendChild(pixel);
+      pixel.style.width = 40;
+      pixel.style.height = 40;
+    }
+  }
+}
+
+btnVQV.addEventListener('click', () => {
+  removeBoard();
+  inputSizeCheck();
+  createBoard();
+});
+
+function generateColorsRadom() {
+  const r = parseInt(Math.random() * 255, 10);
+  const g = parseInt(Math.random() * 255, 10);
+  const b = parseInt(Math.random() * 255, 10);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+firstBlock.style.background = 'black';
+secondBlock.style.background = generateColorsRadom();
+thirdBlock.style.background = generateColorsRadom();
+fourBlock.style.background = generateColorsRadom();
